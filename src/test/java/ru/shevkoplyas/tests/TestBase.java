150999cab0b8b5b0c1ab5a318b9e07572387897d
ShevkoplyasDarya <shevkoplyund@gmail.com>
2021-10-02 16:25:36 +0300

    added allure

--- src/test/java/ru/shevkoplyas/tests/TestBase.java
+++ src/test/java/ru/shevkoplyas/tests/TestBase.java
@@ -1,11 +1,14 @@
 package ru.shevkoplyas.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
         Configuration.baseUrl = "https://demoqa.com";
     }
