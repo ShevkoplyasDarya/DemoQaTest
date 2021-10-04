e6b88f4ceb7f509da15428676db87d4346f1d75e
ShevkoplyasDarya <shevkoplyund@gmail.com>
2021-10-04 11:20:33 +0300

    fixes in TestBase

--- src/test/java/ru/shevkoplyas/tests/TestBase.java
+++ src/test/java/ru/shevkoplyas/tests/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
-
+        Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
