d3c5e2ab8fab2465ed7ddf2380f035387df79d48
ShevkoplyasDarya <shevkoplyund@gmail.com>
2021-10-18 11:35:44 +0300

    removed added properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,14 +2,22 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import helpers.Attach;
+import static config.CredentialsConfig.*;
 
 
 public class TestBase {
+    static CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+
+
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
@@ -20,6 +28,12 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
+
+        String login = credentials.login();
+        String password = credentials.password();
+        String remoteBrowser = System.getProperty("remote", "selenoid.autotests.cloud/wd/hub");
+
+        Configuration.remote = String.format("https://%s:%s@%s", login, password, remoteBrowser);
     }
 
     @AfterEach
